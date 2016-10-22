0d7ff1fe77f88b01ed47800bc1e5ad80d3ec6dbe
kelostrada <kelostrada@gmail.com>
2016-10-22 12:24:17 +0200

    added heroku deployment

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule Zombie.UserSocket do
   # channel "room:*", Zombie.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
