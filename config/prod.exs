974074695652c1dee95e6c2ef21fda2b0e9d3c1c
kelostrada <kelostrada@gmail.com>
2016-10-22 12:39:53 +0200

    fix config

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :zombie, Zombie.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "immense-wave-80129.herokuapp.com", port: 443],
+    force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
@@ -23,7 +24,6 @@ config :logger, level: :info
 # Configure your database
 config :zombie, Zombie.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: [scheme: "https", host: "immense-wave-80129.herokuapp.com", port: 443],
-    force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
